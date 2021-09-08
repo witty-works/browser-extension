b5baf5157f7e33cfb28ea6dedfbad3c27b50a72f
arnaumanyosa <arnau.manyosa@protonmail.com>
2021-09-08 17:36:17 +0200

    now the previous requests are correctly canceled

--- source/shared/ApiServices/useApiResults.tsx
+++ source/shared/ApiServices/useApiResults.tsx
@@ -15,36 +15,39 @@ const useApiResult = (request: IRequest, sendText: any) => {
 
   useEffect(() => {
     const ac = new AbortController();
-    let canceled = false; // Canceled is used to avoid race conditions
-
-    console.log('useApiResult request = ', request);
     setLoading(true);
+
+    request.config = { ...request.config, signal: ac.signal };
 
     fetch(request.url, request.config)
       .then(async (response) => {
-        if (!canceled) {
-          console.log('useApiResult response = ', response);
+        console.log('useApiResult response = ', response);
 
-          setLoading(false);
+        setLoading(false);
 
-          if (response.ok) {
-            const responseResults = await response.json();
-            console.log('useApiResult responseResults = ', responseResults);
-            setEndpointResponse(responseResults);
-            setEndpointError({ detail: [] });
-          } else {
-            setEndpointError(await response.json());
-          }
+        if (response.ok) {
+          const responseResults = await response.json();
+          console.log('useApiResult responseResults = ', responseResults);
+          setEndpointResponse(responseResults);
+          setEndpointError({ detail: [] });
+        } else {
+          setEndpointError(await response.json());
         }
       })
       .catch((error) => {
-        console.log('useApiResult error = ', error);
-        // setError(error); //TODO FIX, this is not received outside
+        // AbortError is created when a request is aborted.
+        // We don't need to shown an error message in this case
+        if (error.name !== 'AbortError') {
+          console.log('useApiResult error = ', error);
+          // setError(error); //TODO FIX, this is not received outside
+        }
+      })
+      .finally(() => {
+        setLoading(false);
       });
+
     return () => {
-      ac.abort(); // Abort both fetches on unmount
-      canceled = true;
-      setLoading(false);
+      ac.abort(); // Abort fetch on unmount
     };
   }, [request.config.body]);
 
