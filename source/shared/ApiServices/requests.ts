fa92c0fe7d3dd556796e1d1d1cb6f9fb4fe15e54
arnaumanyosa <arnau.manyosa@protonmail.com>
2021-09-14 17:04:15 +0200

    id added on current check endpoint calls

--- source/shared/ApiServices/requests.ts
+++ source/shared/ApiServices/requests.ts
@@ -1,11 +1,23 @@
 import { IRequest } from '../types';
-import { BaseUrls } from '../constants';
+import { BaseUrls, StorageKeys } from '../constants';
+import { browser } from 'webextension-polyfill-ts';
 
 let BASE_URL: string = '';
 
 const createUrl = (base: string, path: string): string => `${base}${path}`;
 
 export const setBaseURL = (urlKey: string) => BASE_URL = BaseUrls[urlKey as keyof typeof BaseUrls];
+
+let appID:string = '';
+
+//Get Extension Unique ID
+browser.storage.local
+  .get(StorageKeys.UNIQUE_ID)
+  .then((result) => {
+    appID = result.id;
+  })
+  .catch(error => console.log('getAppID error = ', error));
+
 
 export const getAnalyzedTextResults = (text: string):IRequest => {
   return {
@@ -16,7 +28,7 @@ export const getAnalyzedTextResults = (text: string):IRequest => {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({text: text, lang: 'auto'})
+      body: JSON.stringify({text: text, lang: 'auto', id: appID})
     }
   }
 };
