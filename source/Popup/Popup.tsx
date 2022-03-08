ecad1839ce0c65e2a3faf069d62260919ff25c5a
hellasol <hi@solveig.io>
2022-03-08 16:07:43 +0100

    fix(icon): grey after install

--- source/Popup/Popup.tsx
+++ source/Popup/Popup.tsx
@@ -14,7 +14,7 @@ import { useLog, logTypes } from '../shared/customHooks/useLog';
 import './styles.scss';
 
 const Popup: React.FC = () => {
-  const [enabled, setEnabled] = useState<boolean>();
+  const [enabled, setEnabled] = useState<boolean>(true);
   const { t } = useTranslation(namespaces.pages.popup);
   const log = useLog('Popup');
 
