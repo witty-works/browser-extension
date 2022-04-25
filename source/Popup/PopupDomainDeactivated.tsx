f4d3fd77c93f2f9f09ce7ea0c0bf4d4c665d908a
hellasol <hi@solveig.io>
2022-04-25 09:56:34 +0200

    fix(popup): feedback

--- source/Popup/PopupDomainDeactivated.tsx
+++ source/Popup/PopupDomainDeactivated.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { browser } from 'webextension-polyfill-ts';
 import '../i18n/i18n';
 import './styles.scss';
@@ -10,12 +10,27 @@ import { namespaces } from '../i18n/i18n.constants';
 
 const PopupDomainDeactivated: React.FC = () => {
   const { t } = useTranslation(namespaces.pages.popup);
+  const [pageName, setPageName] = React.useState<string | undefined>('');
+
+  useEffect(() => {
+    browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
+      const domain =
+        tabs[0].url && tabs[0].url.replace('https://www.', '').replace('/', '');
+      domain &&
+        setPageName(
+          domain.split('.')[0].charAt(0).toUpperCase() +
+            domain.split('.')[0].slice(1)
+        );
+    });
+  }, []);
 
   return (
-    <section className='domain-not-supported'>
-      <div className='domain-not-supported-title'>
+    <div className='domain-not-supported'>
+      <div className='domain-not-supported-title-wrapper'>
         <SadFace className='domain-not-supported-icon' />
-        <div className='domain-not-supported-title'>{t('noSupport')}</div>
+        <div className='domain-not-supported-title'>
+          {t('noSupport') + pageName}
+        </div>
       </div>
 
       <div
@@ -37,7 +52,7 @@ const PopupDomainDeactivated: React.FC = () => {
         <EditorButton />
         <div>{t('editor')}</div>
       </div>
-    </section>
+    </div>
   );
 };
 
