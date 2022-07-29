8064e29c4dd55c6c62af332805660f8dcf1de16d
hellasol <hi@solveig.io>
2022-07-29 16:09:43 +0200

    WIP: testing GA

--- __tests__/highlightPlacment.spec.js
+++ __tests__/highlightPlacment.spec.js
@@ -8,8 +8,8 @@ const testTextShort = ' Hey guys, we\'re excited to announce a new front-end dev
 const test = base.extend({
     context: async ({ browserName }, use) => {
         const browserTypes = { chromium } //add firefox
-        const pathToExtension = ('./extension/chrome');
-
+        // const pathToExtension = ('./extension/chrome');
+        const pathToExtension = '/home/runner/work/browser-extension/extension/chrome'; //for GA
         const launchOptions = {
             devtools: false,
             headless: false,
