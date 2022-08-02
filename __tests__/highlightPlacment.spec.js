82f2e62f72da9dc572d7cd9ad67b6ae1a6778159
hellasol <hi@solveig.io>
2022-08-02 16:45:59 +0200

    WIP: testing GA

--- __tests__/highlightPlacment.spec.js
+++ __tests__/highlightPlacment.spec.js
@@ -43,15 +43,14 @@ test.describe('Highlights', () => {
         await page.click('#CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll');
         await page.waitForSelector('#witty-test');
         await page.click('#witty-test');
+
         //write
         // await page.type('#witty-test', 'a');
         await page.waitForTimeout(3000); //wait for api to respond with highlights
 
         await page.locator('#witty-test').screenshot().then(async (screenshot) => {
             expect(screenshot).toMatchSnapshot('witty-form.png', {
-                // maxDiffPixelRatio: 0.05,
-                // threshold: 0.2,
-                // maxDiffPixels: 100,
+                maxDiffPixels: 36000,
                 maxDiffPixelRatio: 0.05
             })
         });
