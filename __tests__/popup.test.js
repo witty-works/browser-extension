88518064d066121aa5b60bddff26b01150bf3fd7
hellasol <hi@solveig.io>
2022-08-03 15:38:17 +0200

    WIP: testing GA

--- __tests__/popup.test.js
+++ __tests__/popup.test.js
@@ -40,7 +40,7 @@ test.setTimeout(120000); //probably not needed here
 test.describe('Popup', () => {
     // // User not logged in
     // test('clicking logo opens a page in another window', async ({ page, context }) => {
-    //     await page.goto(`chrome-extension://libbonaaegmcdbmeefoccaecokjgjmab/popup.html`);
+    //     await page.goto(`chrome-extension://${extensionId}/popup.html`);
     //     await page.waitForSelector('#witty-logo');
     //     await page.click('#witty-logo');
     //     await page.waitForLoadState('networkidle')
@@ -50,7 +50,7 @@ test.describe('Popup', () => {
 
     // test('popup contains three toggles with labels', async ({ page }) => {
     //     //it is 3 toggles, not 5, because it is a chrome page (we dont show the site specific settings)
-    //     await page.goto(`chrome-extension://libbonaaegmcdbmeefoccaecokjgjmab/popup.html`);
+    //     await page.goto(`chrome-extension://${extensionId}/popup.html`);
     //     await page.waitForLoadState('networkidle')
     //     let toggles = await page.$$('.toggle-encloser');
     //     expect(toggles.length).toBe(3);
