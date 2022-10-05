237841dff84cbf56e4d63098af92fb873f440d67
hellasol <hi@solveig.io>
2022-10-05 11:50:10 +0200

    fix: cursor on links

--- source/Popup/Popups/PopupLogin.tsx
+++ source/Popup/Popups/PopupLogin.tsx
@@ -107,7 +107,7 @@ const PopupLogin: React.FC = () => {
           {t('haveAccount')}
           &nbsp;
           <span
-            className='lato-popup-text-purple'
+            className='lato-popup-text-purple cursor-pointer'
             onClick={() => {
               logIn(urls);
             }}
