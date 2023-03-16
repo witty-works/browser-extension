5fb23cbd251ecec474b928ca421b162886d20b6b
hellasol <hi@solveig.io>
2023-03-16 10:13:47 +0100

    fix: screenshots new editor and auto focus issue

--- __tests__/highlightPlacment.spec.js
+++ __tests__/highlightPlacment.spec.js
@@ -6,7 +6,7 @@ const premiumUserEmail = process.env.PREMIUM_TEST_USER_EMAIL;
 const premiumUserPassword = process.env.PREMIUM_TEST_USER_PASSWORD;
 const testText = 'The basics: Witty highlights biased and gendered language in orange: Hey guys, we\'re excited to announce a new front-end developer will assume the leadership role. Taylor has extensive expertise and a strong technical background. Witty highlights inclusive terms in green: We are a creative team. Witty corrects grammar and spelling mistakes. They are highlighted in red: This is a spelling mistacke. Wait... there is more. Witty highlights style issues in yellow: This is actually a very long meeting.'
 const testTextShort = ' Hey guys, we\'re excited to announce a new front-end developer will assume the leadership role.'
-
+const apiWaitTime = 5000;
 const test = base.extend({
     context: async ({ browserName }, use) => {
         const browserTypes = { chromium } //add firefox
@@ -53,14 +53,14 @@ test.describe('Highlights', () => {
 
         await page.waitForLoadState('networkidle')
 
-        await page.waitForSelector('#witty_editor');
-        await page.click('#witty_editor');
-        await page.fill('#witty_editor', testText);
+        await page.waitForSelector('.fr-element');
+        await page.click('.fr-element');
+        await page.fill('.fr-element', testText);
         await page.keyboard.press('Space');
 
-        await page.waitForTimeout(5000); //wait for api to respond with highlights
+        await page.waitForTimeout(apiWaitTime); //wait for api to respond with highlights
 
-        await page.locator('#witty_editor').screenshot().then(async (screenshot) => {
+        await page.locator('.fr-element').screenshot().then(async (screenshot) => {
             expect(screenshot).toMatchSnapshot({
                 maxDiffPixels: 300,
             },
@@ -79,14 +79,14 @@ test.describe('Highlights', () => {
 
         await page.waitForLoadState('networkidle')
 
-        await page.waitForSelector('#witty_editor');
-        await page.click('#witty_editor');
-        await page.fill('#witty_editor', testText);
+        await page.waitForSelector('.fr-element');
+        await page.click('.fr-element');
+        await page.fill('.fr-element', testText);
         await page.keyboard.press('Space');
 
-        await page.waitForTimeout(5000); //wait for api to respond with highlights
+        await page.waitForTimeout(apiWaitTime); //wait for api to respond with highlights
 
-        await page.locator('#witty_editor').screenshot().then(async (screenshot) => {
+        await page.locator('.fr-element').screenshot().then(async (screenshot) => {
             //screenshot accuracy can be adjusted by: maxDiffPixels: 36000, maxDiffPixelRatio: 0.05
             expect(screenshot).toMatchSnapshot({
                 maxDiffPixels: 300,
--- source/shared/DOMutils.ts
+++ source/shared/DOMutils.ts
@@ -1,5 +1,6 @@
 import chroma from 'chroma-js';
 import { getActiveDocument } from '../ContentScript/ContentScriptApp';
+import { getBaseUrls } from './ApiServices/requests';
 import { getDomainWithoutSubdomain } from './utils';
 
 export const isTextArea = (
@@ -15,6 +16,13 @@ export const isInputText = (element: Element): element is HTMLInputElement =>
 
 export const isGoogleDocs = (): boolean => {
   return window.location.href.includes('docs.google.com/document');
+};
+
+export const isWittyEditor = (): boolean => {
+  return (
+    window.location.href.includes('editor') &&
+    window.location.href.includes(getBaseUrls().dashboard)
+  );
 };
 
 export const isGoogleSheets = (): boolean => {
