4764151f1ed5212c9fedb6222a37f0af60c51d2a
hellasol <hi@solveig.io>
2023-03-27 11:10:21 +0200

    fix: top rect position additional scroll

--- source/shared/utils.ts
+++ source/shared/utils.ts
@@ -248,3 +248,19 @@ export const getCorrectedPositionCanvas = (element: HTMLElement) => {
     left: updatedElementRect.left + (scrollLeft ? scrollLeft : 0),
   };
 };
+
+export const getScrollableParentClosestToElement = (element: HTMLElement) => {
+  let style = getComputedStyle(element);
+  const excludeStaticParent = style.position === 'absolute';
+  const overflowRegex = /(auto|scroll)/;
+  if (style.position === 'fixed') return document.body;
+  for (let parent = element; (parent = parent.parentElement as HTMLElement); ) {
+    style = getComputedStyle(parent);
+    if (excludeStaticParent && style.position === 'static') {
+      continue;
+    }
+    if (overflowRegex.test(style.overflow + style.overflowY + style.overflowX))
+      return parent;
+  }
+  return document.body;
+};
