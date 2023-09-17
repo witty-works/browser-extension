4fb505c22aafcf1139a5e790827645933c03fc37
Lukas Kahwe Smith <lukas.smith@witty.works>
2023-09-17 18:52:11 +0200

    updated dev url

--- source/shared/constants.ts
+++ source/shared/constants.ts
@@ -71,7 +71,7 @@ export const BaseUrls: IBaseUrls = {
     dashboard: 'https://dashboard.witty.works/',
   },
   Dev: {
-    api: 'https://dev-54ta5gq-him65foajgj5c.fr-4.platformsh.site/',
+    api: 'https://dev-54ta5gq-2ycn7tlsdvd2o.fr-4.platformsh.site/',
     dashboard: 'https://dev-54ta5gq-56xlfiudba6c2.fr-4.platformsh.site/',
   },
   Local: {
