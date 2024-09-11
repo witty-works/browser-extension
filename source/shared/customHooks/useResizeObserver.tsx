f1f2eff8d6af5840443fec38a9ec5a1988397ecc
Imil Ziyaztdinov <pkzl22@gmail.com>
2024-09-11 15:48:34 +0200

    recalculate input element rect on scroll contaier scroll event (fix sharepoint highlights)

--- source/shared/customHooks/useResizeObserver.tsx
+++ source/shared/customHooks/useResizeObserver.tsx
@@ -6,8 +6,9 @@ import { getScrollableParentClosestToElement } from '../utils';
 export const useResizeObserver = (element: HTMLElement): DOMRect => {
   const [rect, setRect] = useState<DOMRect>(new DOMRect());
   const doc = getActiveDocument().documentElement || getActiveDocument().body;
+  const scrollContainer = getScrollableParentClosestToElement(element);
 
-  const resizeListener = () => {
+  const updateRect = () => {
     const { width, height, top, left } = element.getBoundingClientRect();
 
     const scrollTop = isGoogleDocs()
@@ -17,15 +18,18 @@ export const useResizeObserver = (element: HTMLElement): DOMRect => {
     setRect(new DOMRect(left + doc.scrollLeft, top + scrollTop, width, height));
   };
 
-  const [resizeObserver] = useState(new ResizeObserver(resizeListener));
+  const [resizeObserver] = useState(new ResizeObserver(updateRect));
 
   useEffect(() => {
     resizeObserver.disconnect();
     resizeObserver.observe(element);
     resizeObserver.observe(doc);
 
+    scrollContainer.addEventListener('scroll', updateRect);
+
     return () => {
       resizeObserver.disconnect();
+      scrollContainer.removeEventListener('scroll', updateRect);
     };
   }, [element]);
 
