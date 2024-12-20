5cf31dba54c81c485e83264e19ece1014928564f
Imil Ziyaztdinov <pkzl22@gmail.com>
2024-12-20 14:17:47 +0100

    gdocs fix support script injection

--- source/assets/googleDocsSupport.js
+++ source/assets/googleDocsSupport.js
@@ -1,16 +1,25 @@
-'use strict';
-const injectCode = (e) => {
-    const a = document.createElement('script');
-    (a.textContent = e),
-        (document.head || document.documentElement).appendChild(a);
+const WITTY_EXTENSION_ID = 'hokifickgkhplphjiodbggjmoafhignh';
+window['_docs_annotate_canvas_by_ext'] = WITTY_EXTENSION_ID;
+
+const GDOCS_LINE_COLORS = ["#dd0000", "#4285f4"];
+const GDOCS_RECT_COLORS = ["#fce8e6", "#e8f0fe"];
+
+const wittyIsDisabled = () => {
+    return !document.documentElement.hasAttribute("witty-is-enabled");
 };
-const e = 'hokifickgkhplphjiodbggjmoafhignh';
-injectCode(`(function() {window['_docs_annotate_canvas_by_ext'] ='${e}';})();`);
 
-const injectFile = (e) => {
-    const a = document.createElement('script');
-    const browserApi = (typeof chrome != 'undefined') ? chrome : (typeof browser != 'undefined' ? browser : {});
-    (a.type = 'text/javascript'), (a.src = browserApi.runtime.getURL(e)), document.documentElement.appendChild(a);
-}
+const langCouldBeDetermined = () => {
+    return document.documentElement.getAttribute("witty-could-determine-lang") === "true";
+};
 
-injectFile('assets/googleDocsSpellCheck.js');
+const lineToPrototype = CanvasRenderingContext2D.prototype.lineTo;
+CanvasRenderingContext2D.prototype.lineTo = function (t, s) {
+    if (!this.strokeStyle || wittyIsDisabled() || !langCouldBeDetermined() || !GDOCS_LINE_COLORS.includes(this.strokeStyle.toLowerCase())) {
+        return lineToPrototype.apply(this, arguments)
+    }
+};
+
+const fillRectPrototype = CanvasRenderingContext2D.prototype.fillRect;
+CanvasRenderingContext2D.prototype.fillRect = function (t, e, n, r) {
+    if (!this.fillStyle || wittyIsDisabled() || !langCouldBeDetermined() || !GDOCS_RECT_COLORS.includes(this.fillStyle.toLowerCase())) return fillRectPrototype.apply(this, arguments)
+};
